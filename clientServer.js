2af03844c6c02fbb33bd9948dae3c86a93383978
ujjwal <ujjwalbaranwal221715@gmail.com>
2023-11-27 15:50:29 +0530

    project is completed

--- clientServer.js
+++ clientServer.js
@@ -1,13 +1,161 @@
+//////////////////////////////////////////
+/// inital code
+
 ////////////////////////////////////////
 ///// Client side
+var clientId;
+var gameId;
+var socket;
+var symbol;
 
+const sidebar = document.querySelector(".sidebar");
 const connectBtn = document.querySelector(".ctnBtn");
+const createBtn = document.querySelector(".crtBtn");
+const joinBtn = document.querySelector(".joinBtn");
+const userDetail = document.querySelector(".user_detail");
+const list = document.querySelector(".game-list");
+const board = document.querySelector(".board");
+const cells = document.querySelectorAll(".cell");
 
 connectBtn.addEventListener("click", (e) => {
-  const socket = new WebSocket("ws://localhost:8000");
-
+  socket = new WebSocket("ws://localhost:8000");
   socket.onmessage = onMessage;
   e.target.disabled = true;
 });
 
-function onMessage(msg) {}
+function onMessage(msg) {
+  const data = JSON.parse(msg.data);
+  switch (data.tag) {
+    case "connnected":
+      console.log("connected");
+      console.log({ clientId: data.clientId });
+      clientId = data.clientId;
+      userDetail.textContent = `your user Id : ${data.clientId}`;
+      createBtn.disabled = false;
+      joinBtn.disabled = false;
+      break;
+    case "gamelist":
+      console.log(data.list);
+      const games = data.list;
+      while (list.firstChild) {
+        list.removeChild(list.lastChild);
+      }
+      games.forEach((game) => {
+        const li = document.createElement("li");
+        li.innerText = game;
+        li.style.textAlign = "center";
+        list.appendChild(li);
+        li.addEventListener("click", () => {
+          console.log(`hey my game id is : ${game}`);
+          gameId = game;
+        });
+      });
+      break;
+    case "created":
+      gameId = data.gameId;
+      createBtn.disabled = true;
+      joinBtn.disabled = true;
+      console.log(gameId);
+      break;
+    case "joined":
+      board.style.display = "grid";
+      symbol = data.symbol;
+      if (symbol === "o") {
+        board.classList.add("circle");
+      } else {
+        board.classList.add("cross");
+      }
+      break;
+    case "updateBoard":
+      cells.forEach((cell) => {
+        if (cell.classList.contains("cross")) {
+          cell.classList.remove("cross");
+        } else if (cell.classList.contains("circle")) {
+          cell.classList.remove("circle");
+        }
+      });
+      for (let i = 0; i < 9; i++) {
+        if (data.board[i] == "x") cells[i].classList.add("cross");
+        else if (data.board[i] == "o") cells[i].classList.add("circle");
+      }
+      if (data.isTurn) {
+        makeMove();
+      }
+      return;
+    case "winner":
+      alert(`the winner is ${data.winner}`);
+      break;
+    case "draw":
+      alert(`game is in state draw`);
+      break;
+  }
+}
+
+////////////////////////////////////////////////////////
+///////////////////// logic for create button
+createBtn.disabled = true;
+createBtn.addEventListener("click", () => {
+  socket.send(
+    JSON.stringify({
+      tag: "create",
+      clientId: clientId,
+    })
+  );
+});
+
+//////////////////////////////////////////////////////////
+///////////////////////// Logic for join button
+joinBtn.disabled = true;
+joinBtn.addEventListener("click", () => {
+  socket.send(
+    JSON.stringify({
+      tag: "join",
+      clientId: clientId,
+      gameId: gameId,
+    })
+  );
+});
+/////////////////////////////////////////////////////
+////////////// make move function
+
+function makeMove() {
+  cells.forEach((cell) => {
+    if (
+      !cell.classList.contains("cross") &&
+      !cell.classList.contains("circle")
+    ) {
+      cell.addEventListener("click", cellClicked);
+    }
+  });
+}
+
+function cellClicked(src) {
+  let icon;
+  if (symbol == "x") {
+    icon = "cross";
+  } else {
+    icon = "circle";
+  }
+  src.target.classList.add(icon);
+  const board = [];
+  for (i = 0; i < 9; i++) {
+    if (cells[i].classList.contains("circle")) {
+      board[i] = "o";
+    } else if (cells[i].classList.contains("cross")) {
+      board[i] = "x";
+    } else {
+      board[i] = "";
+    }
+  }
+  cells.forEach((cell) => {
+    cell.removeEventListener("click", cellClicked);
+  });
+  socket.send(
+    JSON.stringify({
+      tag: "moveMade",
+      board,
+      gameId,
+      clientId,
+    })
+  );
+}
--- server.js
+++ server.js
@@ -1,8 +1,174 @@
 ////////////////////////////////////////
 ///// Server side
-
+let clients = {};
+let games = {};
+const WIN_STATES = Array(
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+);
 const http = require("http")
   .createServer()
   .listen(8000, () => {
     console.log("port is 8000");
   });
+
+const server = require("websocket").server;
+
+const socket = new server({ httpServer: http });
+
+socket.on("request", (req) => {
+  const conn = req.accept(null, req.origin);
+  const clientId = Math.floor(Math.random() * (999 - 100)) + 100;
+  clients[clientId] = { conn };
+  conn.send(
+    JSON.stringify({
+      tag: "connnected",
+      clientId: clientId,
+    })
+  );
+  //   console.log(Object.keys(clients));
+  sendAllGame();
+  conn.on("message", onMessage);
+});
+///////////////////////////////////////////
+//////// send all game logic
+function sendAllGame() {
+  //   game = [1, 2, 3];
+  const gameList = [];
+  for (const game in games) {
+    if (games[game].players.length < 2) {
+      gameList.push(game);
+    }
+  }
+  for (c in clients) {
+    clients[c].conn.send(
+      JSON.stringify({
+        tag: "gamelist",
+        list: gameList,
+      })
+    );
+  }
+}
+
+////////////////////////////////
+function onMessage(msg) {
+  const data = JSON.parse(msg.utf8Data);
+  switch (data.tag) {
+    case "create":
+      const gameId = Math.floor(Math.random() * (999 - 100)) + 100;
+      const board = ["", "", "", "", "", "", "", "", ""];
+      const playerCreate = {
+        clientId: data.clientId,
+        symbol: "x",
+        isTurn: true,
+      };
+      const players = Array(playerCreate);
+      games[gameId] = {
+        board,
+        players,
+      };
+      clients[data.clientId].conn.send(
+        JSON.stringify({
+          tag: "created",
+          gameId,
+        })
+      );
+      sendAllGame();
+      break;
+    case "join":
+      const playerJoin = {
+        clientId: data.clientId,
+        symbol: "o",
+        isTurn: false,
+      };
+      console.log(data.gameId);
+      games[data.gameId].players.push(playerJoin);
+      sendAllGame();
+      games[data.gameId].players.forEach((player) => {
+        clients[player.clientId].conn.send(
+          JSON.stringify({
+            tag: "joined",
+            gameId: data.gameId,
+            symbol: player.symbol,
+          })
+        );
+      });
+      updateGame(data.gameId);
+      break;
+    case "moveMade":
+      games[data.gameId].board = data.board;
+      const isWinner = winState(data.gameId);
+      const isDraw = stateDraw(data.gameId);
+      if (isWinner) {
+        games[data.gameId].players.forEach((player) => {
+          clients[player.clientId].conn.send(
+            JSON.stringify({
+              tag: "winner",
+              winner: player.symbol,
+            })
+          );
+        });
+      } else if (isDraw) {
+        games[data.gameId].players.forEach((player) => {
+          clients[player.clientId].conn.send(
+            JSON.stringify({
+              tag: "draw",
+            })
+          );
+        });
+      } else {
+        games[data.gameId].players.forEach((player) => {
+          player.isTurn = !player.isTurn;
+        });
+      }
+      updateGame(data.gameId);
+      break;
+  }
+}
+/////////////////////////////////////////////
+////////////// Update game
+function updateGame(gameId) {
+  games[gameId].players.forEach((player) => {
+    clients[player.clientId].conn.send(
+      JSON.stringify({
+        tag: "updateBoard",
+        isTurn: player.isTurn,
+        board: games[gameId].board,
+      })
+    );
+  });
+}
+
+/////////// win state
+function winState(gameId) {
+  return WIN_STATES.some((row) => {
+    return (
+      row.every((cell) => {
+        return games[gameId].board[cell] == "x";
+      }) ||
+      row.every((cell) => {
+        return games[gameId].board[cell] == "o";
+      })
+    );
+  });
+}
+
+////////// draw state
+function stateDraw(gameId) {
+  return WIN_STATES.every((row) => {
+    return (
+      row.some((cell) => {
+        return games[gameId].board[cell] == "x";
+      }) &&
+      row.some((cell) => {
+        return games[gameId].board[cell] == "o";
+      })
+    );
+  });
+}
